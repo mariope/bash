e7ba72cfb4f25849bc13c555daa269301dc64d29
mariope <mario.pisa@gmail.com>
2017-01-22 21:33:00 +0100

    Modify getBarrons.sh and add AWS-SES.sh

---
+++ AWS-SES.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TO="Email To <mario.pisa@gmail.com>"
+FROM="Email From <mario.pisa@gmail.com>"
+SUBJECT="Hey SES"
+MESSAGE="This work!"
+
+date="$(date -R)"
+priv_key="$AWS_SECRET_KEY"
+access_key="$AWS_ACCESS_KEY"
+signature="$(echo -n "$date" | openssl dgst -sha256 -hmac "$priv_key" -binary | base64 -w 0)"
+auth_header="X-Amzn-Authorization: AWS3-HTTPS AWSAccessKeyId=$access_key, Algorithm=HmacSHA256, Signature=$signature"
+endpoint="https://email.us-east-1.amazonaws.com/"
+mime_version="MIME-Version: 1.0"
+
+action="Action=SendEmail"
+source="Source=$FROM"
+to="Destination.ToAddresses.member.1=$TO"
+subject="Message.Subject.Data=$SUBJECT"
+xmllint --html --xpath "//body" barronsCOT.html 2>/dev/null >> barronsCOT2.html
+message="Message.Body.Html.Data="'{"image" : "'"$(base64 barronsCOT2.html)"'"}'
+
+curl -v -X POST -H "Date: $date" -H "$content_type" -H "$mime_version" -H "$auth_header" --data-urlencode "$message" --data-urlencode "$to" --data-urlencode "$source" --data-urlencode "$action" --data-urlencode "$subject" "$endpoint"
--- getBarrons.sh
+++ getBarrons.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 ./getBarrons.R > getBarrons.log
+# python -m premailer -f barronsCOT.html -o barronsCOT2.html
+# xmllint --html --xpath "//body" barronsCOT.html 2>/dev/null >> barronsCOT2.html
 
-# send output by sendgrid mail
-SGTO='mario.pisa@gmail.com'
-SGTONAME='Mario'
-SGSUBJECT='Output from getBarrons.R '$(date)
-SGFROM=mariope.develop@gmail.com
-SGTEXT=$(date)'<p>'$(cat getBarrons.log)
-SGUSER=$SENDGRID_USERNAME
-SGPASS=$SENDGRID_PASSWORD
-curl -d "to=${SGTO}&toname=${SGTONAME}&subject=${SGSUBJECT}&text=${SGTEXT}&from=${SGFROM}&api_user=${SGUSER}&api_key=${SGPASS}" https://api.sendgrid.com/api/mail.send.json
+# # send output by sendgrid mail
+# SGTO='mario.pisa@gmail.com'
+# SGTONAME='Mario'
+# SGSUBJECT='Output from getBarrons.R '$(date)
+# SGFROM=mariope.develop@gmail.com
+# SGTEXT='hola'
+# SGUSER=$SENDGRID_USERNAME
+# SGPASS=$SENDGRID_PASSWORD
+# curl -d "to=${SGTO}&toname=${SGTONAME}&subject=${SGSUBJECT}&html=${SGTEXT}&files=${'barronsCOT.html'}&from=${SGFROM}&api_user=${SGUSER}&api_key=${SGPASS}" https://api.sendgrid.com/api/mail.send.json
+./AWS-SES.sh
